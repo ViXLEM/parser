67870b395e2df67f94bc0e3c51a2ae368198be72
ViXLEM <victor.shutenko@gmail.com>
2017-11-14 22:31:59 +0200

    fix code style

--- app.py
+++ app.py
@@ -1,32 +1,25 @@
 import os
 
 from celery_config import init_celery
-from config import DATABASE_URL
 from datetime import timedelta
 from flask import Flask
 
 
 app = Flask(__name__)
-app.config.update(
-    CELERY_BROKER_URL='redis://localhost:6379',
-    CELERY_RESULT_BACKEND='redis://localhost:6379',
-	CELERY_IMPORTS=("parser",),
-	CELERYBEAT_SCHEDULE = {
-    'print': {
-        'task': 'parser.get_goods_data',
-        'schedule': timedelta(days=1),
-        },
-    }
-)
+app.config.update(CELERY_BROKER_URL='redis://localhost:6379', CELERY_RESULT_BACKEND='redis://localhost:6379',
+                  CELERY_IMPORTS="parser",
+                  CELERYBEAT_SCHEDULE={'print': {'task': 'parser.get_goods_data', 'schedule': timedelta(days=1)}}
+                  )
 
 celery = init_celery(app)
 
+
 @app.route("/")
 def index():
-	"""Return simple string."""
-	return "Main page text"
+    """Return simple string."""
+    return "Main page text"
 
 
 if __name__ == "__main__":
-	port = int(os.environ.get("PORT", 5000))
-	app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
