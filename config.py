05765e3eadc8c1c00dac8bee38dbe6a4ef761d54
DrEdi <grigoriy.rubel@gmail.com>
2017-11-14 19:38:45 +0200

    Add celery integration

---
+++ config.py
@@ -0,0 +1,4 @@
+import os
+
+DATABASE_URL = os.environ.get('DATABASE_URL')
+SECRET_URL = os.environ.get('SECRET_URL')
--- parser.py
+++ parser.py
@@ -1,6 +1,11 @@
 import grequests
 
+from app import celery
+from config import SECRET_URL
 from bs4 import BeautifulSoup
+from celery.utils.log import get_task_logger
+
+logger = get_task_logger(__name__)
 
 
 def parse_data(html_code):
@@ -13,18 +18,22 @@ def parse_data(html_code):
         product_name = product.find_all('a')[1].text
         product_price = product.find('div', class_='price').text.replace(' ', '')[1:]  # del white spaces and '/n'
         data_list.append({'name': product_name, 'price': product_price})
+    logger.info('End parse HTML code...')
     return data_list
 
 
+@celery.task
 def get_goods_data():
     """Request categories url and return list of goods."""
 
+    logger.info('Starting fetch data...')
     list_catalog_url = ['https://megamarket.ua/ua/catalogue/category/{0}?'
                         'show=48000'.format(x) for x in range(1010, 1120, 10)]
     requests_gen = (grequests.get(url) for url in list_catalog_url)
     response_list = grequests.map(requests_gen)
     data = []
+    logger.info('Starting parse data...')
     for response in response_list:
         catalog_data = parse_data(response.text)
         data.extend(catalog_data)
-    return data
+    grequests.post(SECRET_URL, data=data)
